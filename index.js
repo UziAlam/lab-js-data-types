a4ee97c2156174d0ce4bb8a4a63c8afb5e97c0a6
UziAlam <u.alam@live.com.au>
2025-11-03 20:12:27 +0100

    Solved Lab

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-/*******************************************
+/********************************************************************************
     Iteration 1.1 | Tongue Twister
-*******************************************/
+*********************************************************************************/
 const s1 = "Fred";
 const s2 = "fed";
 const s3 = "Ted";
@@ -9,48 +9,49 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5}`;
 
 // Print out the concatenated string
+console.log(tongueTwister)
 
 
-
-
-/*******************************************
+/**********************************************************************************
     Iteration 1.2 | Camel Tail
-*******************************************/
+***********************************************************************************/
 const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const CapA = part1.slice(-1).toUpperCase()
+const CapB = part2.slice(-1).toUpperCase()
 
 // Print the cameLtaiL-formatted string
+console.log(part1.slice(0,3) + CapA);
+console.log(part2.slice(0,5) + CapB)
 
 
-
-
-/*******************************************
+/*********************************************************************************
     Iteration 2.1 | Calculate Tip
-*******************************************/
+**********************************************************************************/
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+let tipamount = billTotal * 0.15;
 
 // Print out the tipAmount
+console.log(tipamount);
 
 
 
-
-/*******************************************
+/***********************************************************************************
     Iteration 2.2 | Generate Random Number
-*******************************************/
+************************************************************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floor(Math.random() * 10) + 1;
 
 // Print the generated random number
-
+console.log(randomNumber);
 
 
 /*******************************************
